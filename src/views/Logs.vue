a094f7d379febcec4eca11227fdb5d9fd891747f
zy414563492 <414563492@qq.com>
2021-12-05 23:44:30 +0900

    Update Logs.vue

--- src/views/Logs.vue
+++ src/views/Logs.vue
@@ -8,6 +8,10 @@
       :items="logs"
       class="elevation-1"
     >
+      <template v-slot:[`item.deviceID`]="{ item }">
+        {{ showDetailByID(item.deviceID) }}
+      </template>
+
       <template v-slot:[`item.temperature`]="{ item }">
         <v-chip
           :color="getColor(item.temperature)"
@@ -64,11 +68,20 @@
                       cols="12"
                     >
                       <v-select
-                        v-model="editedItem.to_device"
+                        v-model="editedItem.deviceID"
                         :items="devices"
+                        item-text="detail.device_id"
+                        item-value="id"
                         label="デバイスID"
                         outlined
-                      ></v-select>
+                      >
+                        <!-- <template slot="selection" slot-scope='{ item }'>
+                          {{ `${ item.detail.device_id } (${ item.detail.is_entrance }, ${ item.detail.is_using })` }}
+                        </template>
+                        <template slot="item" slot-scope='{ item }'>
+                          {{ `${ item.detail.device_id } (${ item.detail.is_entrance }, ${ item.detail.is_using })` }}
+                        </template> -->
+                      </v-select>
                     </v-col>
                   </v-row>
                   <v-row>
@@ -79,6 +92,7 @@
                         v-model="editedItem.log_id"
                         label="ログID"
                         outlined
+                        :disabled="editedIndex !== -1"
                       ></v-text-field>
                     </v-col>
                   </v-row>
@@ -205,185 +219,282 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      name: '検温記録',
-      dialog: false,
-      dialogDelete: false,
-      headers: [
-        { text: 'デバイスID', value: 'to_device' },
-        { text: 'ログID', value: 'log_id' },
-        { text: '時間', value: 'time' },
-        { text: '体温', value: 'temperature' },
-        { text: 'ブラックリスト検出', value: 'is_blacklist' },
-        { text: '操作', value: 'actions', sortable: false },
-      ],
-      devices: [],
-      logs: [],
-      blacklist_state: [
-        { state: true, state_name: '◯' },
-        { state: false, state_name: '✖️' }
-      ],
-      editedIndex: -1,
-      editedItem: {
-        to_device: null,
-        log_id: null,
-        time: null,
-        temperature: null,
-        is_blacklist: false,
-      },
-      defaultItem: {
-        to_device: null,
-        log_id: null,
-        time: null,
-        temperature: null,
-        is_blacklist: false,
-      },
-    }),
+import { DataStore } from '@aws-amplify/datastore'
+import { Device, Log } from '@/models'
 
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? '模擬検温記録を追加' : '模擬検温記録を更新'
-        // return 'New Item'
-      },
+export default {
+  data: () => ({
+    name: '検温記録',
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      { text: 'デバイスID', value: 'deviceID' },
+      { text: 'ログID', value: 'log_id' },
+      { text: '時間', value: 'time' },
+      { text: '体温', value: 'temperature' },
+      { text: 'ブラックリスト検出', value: 'is_blacklist' },
+      { text: '操作', value: 'actions', sortable: false },
+    ],
+    devices: [],
+    // devices中元素构造如下
+    // device: {
+    //   id: null,
+    //   detail: {
+    //     device_id: null,
+    //     is_entrance: null,
+    //     is_using: null,
+    //   },
+    // },
+    logs: [],
+    blacklist_state: [
+      { state: true, state_name: '◯' },
+      { state: false, state_name: '✖️' }
+    ],
+    editedIndex: -1,
+    editedItem: {
+      deviceID: null,
+      log_id: null,
+      time: null,
+      temperature: null,
+      is_blacklist: false,
+    },
+    defaultItem: {
+      deviceID: null,
+      log_id: null,
+      time: null,
+      temperature: null,
+      is_blacklist: false,
+    },
+  }),
+
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? '模擬検温記録を追加' : '模擬検温記録を更新'
+      // return 'New Item'
+    },
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created () {
+    this.getDevices()
+    this.initialize()
+  },
+
+  methods: {
+    // async initialize () {
+    //   await this.$axios
+    //   .post('/api/get_logs')
+    //   .then((res) => {
+    //     console.log(res.data)
+    //     this.logs = res.data.log_list
+    //   })
+    //   .catch(err => console.log(err))
+    // },
+
+    async initialize () {
+      var init_logs = await DataStore.query(Log)
+      init_logs.forEach(log => this.logs.push(log))
+      console.log(this.logs)
     },
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
+    async addLog (item) {
+      await this.$axios({
+        method: 'post',
+        url: '/api/add_log',
+        withCredentials: true,
+        data: JSON.stringify(item)
+      }).then((res) => {
+        console.log(res)
+      })
     },
 
-    created () {
-      this.initialize()
-      this.getDevices()
+    async removeLog (item) {
+      await this.$axios({
+        method: 'post',
+        url: '/api/remove_log',
+        withCredentials: true,
+        data: JSON.stringify(item)
+      }).then((res) => {
+        console.log(res)
+      })
     },
 
-    methods: {
-      async initialize () {
-        await this.$axios
-        .post('/api/get_logs')
-        .then((res) => {
-          console.log(res.data)
-          this.logs = res.data.log_list
-        })
-        .catch(err => console.log(err))
-      },
+    // 所属デバイスの選択肢を取得する
+    // async getDevices () {
+    //   await this.$axios
+    //   .post('/api/get_devices_id')
+    //   .then((res) => {
+    //     console.log(res.data)
+    //     this.devices = res.data.devices
+    //   })
+    //   .catch(err => console.log(err))
+    // },
 
-      async addLog (item) {
-        await this.$axios({
-          method: 'post',
-          url: '/api/add_log',
-          withCredentials: true,
-          data: JSON.stringify(item)
-        }).then((res) => {
-          console.log(res)
-        })
-      },
+    async getDevices () {
+      var select_devices = await DataStore.query(Device)
 
-      async removeLog (item) {
-        await this.$axios({
-          method: 'post',
-          url: '/api/remove_log',
-          withCredentials: true,
-          data: JSON.stringify(item)
-        }).then((res) => {
-          console.log(res)
-        })
-      },
-
-      // 所属デバイスの選択肢を取得する
-      async getDevices () {
-        await this.$axios
-        .post('/api/get_devices_id')
-        .then((res) => {
-          console.log(res.data)
-          this.devices = res.data.devices
-        })
-        .catch(err => console.log(err))
-      },
-
-      // get current time
-      getdateTime () {
-        var _this = this;
-        let yy = new Date().getFullYear();
-        let mm = new Date().getMonth() + 1;
-        let dd = new Date().getDate();
-        let hh = new Date().getHours();
-        let mf =
-          new Date().getMinutes() < 10
-            ? "0" + new Date().getMinutes()
-            : new Date().getMinutes();
-        let ss =
-          new Date().getSeconds() < 10
-            ? "0" + new Date().getSeconds()
-            : new Date().getSeconds();
-        let gettime = yy + "-" + mm + "-" + dd + " " + hh + ":" + mf + ":" + ss;
-        return gettime;
-      },
-
-      getColor (temperature) {
-        if (temperature > 37.5) return 'red'
-        else if (temperature > 37.0) return 'orange'
-        else return 'green'
-      },
-
-      getIconColor (is_blacklist) {
-        if (is_blacklist) return 'red'
-        else return 'green'
-      },
-
-      editItem (item) {
-        this.editedIndex = this.logs.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        this.editedIndex = this.logs.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
-
-      deleteItemConfirm () {
-        // remove from database
-        this.removeLog(this.editedItem)
-
-        // remove from frontend table
-        this.logs.splice(this.editedIndex, 1)
-        this.closeDelete()
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.logs[this.editedIndex], this.editedItem)
-        } else {
-          this.logs.push(this.editedItem)
-        }
-        // デバイスの追加
-        this.addLog(this.editedItem)
-
-        this.close()
-      },
+      select_devices.forEach(device => this.devices.push({
+        id: device.id,
+        detail: {
+          device_id: device.device_id,
+          is_entrance: device.is_entrance,
+          is_using: device.is_using,
+        },
+      }))
+      // console.log(this.users)
     },
-  }
+
+    // get current time
+    getdateTime () {
+      var _this = this;
+      let yy = new Date().getFullYear()
+      // let mm = new Date().getMonth() + 1
+      let mm = 
+        new Date().getMonth() < 10
+          ? "0" + (new Date().getMonth() + 1)
+          : new Date().getMonth() + 1
+      // let dd = new Date().getDate()
+      let dd = 
+        new Date().getDate() < 10
+          ? "0" + new Date().getDate()
+          : new Date().getDate()
+      // let hh = new Date().getHours()
+      let hh = 
+        new Date().getHours() < 10
+          ? "0" + new Date().getHours()
+          : new Date().getHours()
+      let mf =
+        new Date().getMinutes() < 10
+          ? "0" + new Date().getMinutes()
+          : new Date().getMinutes()
+      let ss =
+        new Date().getSeconds() < 10
+          ? "0" + new Date().getSeconds()
+          : new Date().getSeconds()
+      // let gettime = yy + "-" + mm + "-" + dd + " " + hh + ":" + mf + ":" + ss
+      // for AWS Datetime
+      let gettime = yy + "-" + mm + "-" + dd + "T" + hh + ":" + mf + ":" + ss + "Z"
+      return gettime
+    },
+
+    getColor (temperature) {
+      if (temperature > 37.5) return 'red'
+      else if (temperature > 37.0) return 'orange'
+      else return 'green'
+    },
+
+    getIconColor (is_blacklist) {
+      if (is_blacklist) return 'red'
+      else return 'green'
+    },
+
+    editItem (item) {
+      this.editedIndex = this.logs.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      this.editedIndex = this.logs.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    // DELETE
+    async deleteItemConfirm () {
+      // backend
+      // this.removeLog(this.editedItem)
+
+      const targetItem = await DataStore.query(Log, this.logs[this.editedIndex].id)
+      DataStore.delete(targetItem)
+      console.log(`Item【${targetItem.log_id}】deleted.`)
+
+      // frontend
+      this.logs.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    async save () {
+      // UPDATE
+      if (this.editedIndex > -1) {
+        // Object.assign(this.logs[this.editedIndex], this.editedItem)
+        // backend
+        const targetItem = await DataStore.query(Log, this.logs[this.editedIndex].id)
+
+        var updatedItem = Log.copyOf(targetItem, updated => {
+          // 不改log_id，因此在修改的UI中也禁用掉了log_id输入框
+          updated.temperature = Number(this.editedItem.temperature)
+          updated.time = this.editedItem.time
+          updated.is_blacklist = this.editedItem.is_blacklist
+          updated.deviceID = this.editedItem.deviceID
+        })
+
+        await DataStore.save(updatedItem)
+        console.log(`Item【${targetItem.log_id}】updated.`)
+
+        // frontend
+        this.logs.splice(this.editedIndex, 1, updatedItem)
+
+      // CREATE
+      } else {
+        // backend
+        const createdItem = await DataStore.save(
+          // new Log(this.editedItem)
+          new Log({
+            "log_id": this.editedItem.log_id,
+            "temperature": Number(this.editedItem.temperature),
+            "time": this.editedItem.time,
+            "is_blacklist": this.editedItem.is_blacklist,
+            "deviceID": this.editedItem.deviceID,
+          })
+        )
+        console.log(`Item【${this.editedItem.log_id}】created.`)
+        // console.log(`createdItem_id = ${createdItem.id}`)
+
+        // frontend
+        this.logs.push(createdItem)
+        console.log(this.logs)
+        // this.logs.push(this.editedItem)
+      }
+      // デバイスの追加
+      // this.addLog(this.editedItem)
+      this.close()
+    },
+
+    // find specific item in object array
+    showDetailByID (id) {
+      let obj = this.devices.find(o => o.id === id)
+      if (obj !== undefined) {
+        return `${ obj.detail.device_id }`
+        // return `${ obj.detail.device_id } (${ obj.detail.is_entrance }, ${ obj.detail.is_using })`
+      } else {
+        return ''
+      }
+    },
+  },
+}
 </script>

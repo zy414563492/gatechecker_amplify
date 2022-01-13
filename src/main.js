0ccc478107655f8d871ff47a390d81a61ec4eff9
zy414563492 <414563492@qq.com>
2022-01-13 23:25:57 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ import Amplify, { AuthModeStrategyType } from "aws-amplify"
 import awsconfig from './aws-exports'
 import "@aws-amplify/ui-vue/styles.css"
 
-import { gql } from 'graphql-tag'
-
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
